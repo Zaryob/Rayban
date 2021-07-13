27abce18a874101fa0860e35103fa23ce85c8f67
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-07-13 11:48:53 +0300

    Form 1 resources fixed.

--- FrictionAndWearTest/Form1.Designer.cs
+++ FrictionAndWearTest/Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace FrictionAndWearTest
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::FrictionAndWearTest.Properties.Resources.Image1;
             this.pictureBox1.Location = new System.Drawing.Point(40, 120);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(353, 302);
